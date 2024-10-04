f393a344bc3ae9eb5870e5b0f12663088d2b8902
LeonardoJ-Senai <leonardo.jose@edu.sc.senai.br>
2024-10-04 17:33:18 -0300

    v 04.10

--- Frontend/public/JS/script.js
+++ Frontend/public/JS/script.js
@@ -6,12 +6,69 @@ async function getTarefas(){
         const resposta = await fetch(API_URL)
         const lista_tarefas = await resposta.json()
         console.log(lista_tarefas)
-        
     }
     catch(erro){
         const message_error = "Erro ao abrir as tarefas: " + erro
-        console.log(getTarefas(message_error))
+        console.log(message_error)
     }
 }
 
-console.log(getTarefas())
+// Função para postar novas tarefas
+async function postTarefas(novaTarefa){
+    try{
+        const resposta = await fetch(API_URL,{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            // Formato {"nome":"nometarefa","status":"statustarefa"}
+            body:JSON.stringify(novaTarefa)
+        })
+    }
+    catch(erro){
+        console.log("Erro ao adicionar uma tarefa: "+erro)
+    }
+}
+
+// Função para deletar tarefas por id
+async function deleteTarefas(id){
+    try {
+        const resposta = await fetch(`${API_URL}/${id}`,{
+            method:"DELETE"
+        })
+    } catch (erro) {
+        console.log("Erro ao adicionar uma tarefa: "+erro)
+    }
+}
+
+// Deletar todas as tarefas
+async function limparTarefas(){
+    try {
+        const resposta = await fetch(API_URL,{
+            method:"DELETE"})
+    } catch (erro) {
+        console.log("Erro ao adicionar uma tarefa: "+erro)
+    }
+}
+
+
+document.getElementById("buttonGet").onclick=async()=>{
+    await getTarefas()
+}
+document.getElementById("buttonPost").onclick=async()=>{
+    const novaTarefa = {"nome":"nometarefa","status":"statustarefa"}
+    await postTarefas(novaTarefa)
+}
+document.getElementById("buttonPut").onclick=async()=>{
+    console.log("Ainda não criado")
+}
+document.getElementById("buttonDelete").onclick=async()=>{
+    const id = 0
+    await deleteTarefas(id)
+}
+document.getElementById("buttonDeleteAll").onclick=async()=>{
+    await limparTarefas()
+}
+
+
+
+
+//document.addEventListener("DOMContentLoaded",getTarefas())
